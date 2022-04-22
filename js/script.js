f1438fbec35311d1853335bc2cdb9662061f08e3
chintriago <chrisintriago94@gmail.com>
2022-04-21 21:52:43 -0700

    finished website.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log("script succesfull.")
+const form = document.getElementById("form-js");
+const article1 = document.getElementById("article-1-js");
+const article2 = document.getElementById("article-2-js");
+const span = document.getElementById("span-js");
+const inputList = document.querySelectorAll("input");
+const inputListContainer = document.getElementById("btns-container-js");
+
+// Function loops through inputs to see which
+// input was clicked, then when on that input
+// the html elements get changed.
+function clickListener() {
+    for (let i = 0; i < inputList.length; i++) {
+        if (inputList[i] === document.activeElement) {
+            let num = i + 1;
+            let str = num.toString();
+            span.textContent = str;
+            form.addEventListener("submit", function (event) {
+                event.preventDefault();
+                article1.classList.add("remove-element");
+                article2.classList.remove("remove-element");
+            });
+        }
+    }
+};
+
+// When div container for inputs gets clicked,
+// clickListener function gets called.
+inputListContainer.addEventListener("click", function () {
+    clickListener();
+});
